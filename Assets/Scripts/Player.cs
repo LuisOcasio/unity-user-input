27cc6535e669a94714a15dad9e68beaceefce3d8
Luis <ocasio.dev@gmail.com>
2020-11-26 23:46:32 -0500

    Initial check-in of module User-Input-Action-Project

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //initialize functionality for player to Jump
+        if (Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            Debug.Log("Space bar pressed");
+        }    
+    }
+}
